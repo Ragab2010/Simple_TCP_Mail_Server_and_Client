e8a939be1d2a803b9562777e81eba9f9b642cae5
Ragab2010 <ragabmx2002@gmail.com>
2024-04-01 17:46:09 +0200

    add new method to JsonHndler (serializeToJson_v2 ,deserializeFromJson_v2)

--- server/JsonHandler.cpp
+++ server/JsonHandler.cpp
@@ -18,6 +18,23 @@ std::string JsonHandler::serializeToJson(const std::string& requestType, const s
     delete writer;
     return os.str();
 }
+std::string JsonHandler::serializeToJson_v2(const std::string& requestType, const std::string& username, const std::string& receiver, const std::string& message) {
+    Json::Value root;
+    root["requestType"] = requestType;
+    root["username"] = username;
+    root["receiver"] = receiver;
+    root["message"] = message;
+
+    Json::StreamWriterBuilder builder;
+    builder["indentation"] = "";
+    std::string jsonData = Json::writeString(builder, root);
+    return jsonData;
+    /*******************************/
+    // std::stringstream ss
+    // ss<<root;
+    // return ss.str();
+    /*******************************/
+}
 
 std::tuple<std::string, std::string, std::string, std::string> JsonHandler::deserializeFromJson(const std::string& jsonString) {
     std::string requestType, username, receiver, message;
@@ -27,6 +44,28 @@ std::tuple<std::string, std::string, std::string, std::string> JsonHandler::dese
     std::string errs;
     if (!Json::parseFromStream(builder, is, &root, &errs)) {
         std::cerr << "Error parsing JSON: " << errs << std::endl;
+        return std::make_tuple("", "", "", "");
+    }
+
+    if (root.isMember("requestType") && root["requestType"].isString())
+        requestType = root["requestType"].asString();
+    if (root.isMember("username") && root["username"].isString())
+        username = root["username"].asString();
+    if (root.isMember("receiver") && root["receiver"].isString())
+        receiver = root["receiver"].asString();
+    if (root.isMember("message") && root["message"].isString())
+        message = root["message"].asString();
+
+    return std::make_tuple(requestType, username, receiver, message);
+}
+
+std::tuple<std::string, std::string, std::string, std::string> JsonHandler::deserializeFromJson_v2(const std::string& jsonString) {
+    std::string requestType, username, receiver, message;
+    Json::Value root;
+    Json ::Reader reader;
+    bool parsingSuccessful = reader.parse(jsonString, root);
+    if (!parsingSuccessful) {
+        std::cerr << "Error parsing JSON: "<< std::endl;
         return std::make_tuple("", "", "", "");
     }
 
