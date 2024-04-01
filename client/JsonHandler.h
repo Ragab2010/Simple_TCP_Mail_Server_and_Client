0031368f846b6b6e2816e8c1222c9c30d67cdb9f
Ragab2010 <ragabmx2002@gmail.com>
2024-04-01 17:45:59 +0200

    add new method to JsonHndler (serializeToJson_v2 ,deserializeFromJson_v2)

--- client/JsonHandler.h
+++ client/JsonHandler.h
@@ -8,6 +8,8 @@ class JsonHandler {
 public:
     static std::string serializeToJson(const std::string& requestType, const std::string& username, const std::string& receiver, const std::string& message);
     static std::tuple<std::string, std::string, std::string, std::string> deserializeFromJson(const std::string& jsonString);
+    static std::string serializeToJson_v2(const std::string& requestType, const std::string& username, const std::string& receiver, const std::string& message);
+    static std::tuple<std::string, std::string, std::string, std::string> deserializeFromJson_v2(const std::string& jsonString);
 };
 
 #endif /* JSON_HANDLER_H */
